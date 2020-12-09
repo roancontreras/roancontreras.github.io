31ff7847247d8a4c22d7a44ff24dffa098c24db8
Roan Contreras <toadfrogs@hotmail.com>
2020-12-10 01:03:39 +0800

    Added Magic Grid plugin

--- js/application.js
+++ js/application.js
@@ -1,38 +1,44 @@
-// "use strict";
+"use strict";
 
-// (function() 
-// {
-//     let m_pages = ["home", "portfolio"];
-//     let m_pageIndex = 0;
+(function() 
+{
+    $(document).ready(function()
+    {
+        let magicGrid = new MagicGrid({
+            container: '.container',
+            animate: false,
+            gutter: 5,
+            static: true,
+            useMin: true
+          });
+        
+          magicGrid.listen();
 
-//     $(document).ready(function()
-//     {
-//         $('body').on('contextmenu', function(e){ e.preventDefault(); });
+        $('body').on('contextmenu', function(e){ e.preventDefault(); });
+        // for (var i = 0; i < m_pages.length; i++)
+        // {
+        //     $("nav ul").append("<li data-pindex=\"" + i.toString() + "\">" + m_pages[i] + "</li>");
+        //     var _ulBtn = "nav ul li[data-pindex=\"" + i.toString() +  "\"]";
+        //     $(_ulBtn).click(function()
+        //     {
+        //         let _getIndex = $(this).attr("data-pindex");
+        //         if (_getIndex == m_pageIndex)
+        //             return;
 
-//         for (var i = 0; i < m_pages.length; i++)
-//         {
-//             $("nav ul").append("<li data-pindex=\"" + i.toString() + "\">" + m_pages[i] + "</li>");
-//             var _ulBtn = "nav ul li[data-pindex=\"" + i.toString() +  "\"]";
-//             $(_ulBtn).click(function()
-//             {
-//                 let _getIndex = $(this).attr("data-pindex");
-//                 if (_getIndex == m_pageIndex)
-//                     return;
+        //         m_pages.forEach(_p =>
+        //             {
+        //                 let _isCurrentPage = m_pages.indexOf(_p) ==  _getIndex;
 
-//                 m_pages.forEach(_p =>
-//                     {
-//                         let _isCurrentPage = m_pages.indexOf(_p) ==  _getIndex;
+        //                 if (!_isCurrentPage)
+        //                     $("#" + _p).removeClass("active");
+        //                 else
+        //                     $("#" + _p).addClass("active");
 
-//                         if (!_isCurrentPage)
-//                             $("#" + _p).removeClass("active");
-//                         else
-//                             $("#" + _p).addClass("active");
+        //                 $("#" + _p).css({"display" : (_isCurrentPage ? "flex" : "none")});
+        //             });
 
-//                         $("#" + _p).css({"display" : (_isCurrentPage ? "flex" : "none")});
-//                     });
-
-//                 m_pageIndex = _getIndex;
-//             });
-//         }
-//     });
-// })();
+        //         m_pageIndex = _getIndex;
+        //     });
+        // }
+    });
+})();
