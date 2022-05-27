959a0ef86c9d8eed74267f58a3897f4ba05d55e9
Roan Contreras <roan.contreras@hotmail.com>
2022-05-27 22:15:11 +0800

    Games listing

---
+++ list.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var data = {
+    "g":
+    [
+    {"star":0, "title":"QR Tale", "desc":"Story driven with a QR scanner.", "link":"https://toadietechnika.itch.io/qr-tale"},
+    {"star":0, "title":"Love Nightmare", "desc":"Collect all the hearts, in ups and downs.", "link":"https://www.newgrounds.com/portal/view/832657"},
+    {"star":0, "title":"Around the World", "desc":"By encoding countries.", "link":"http://roancontreras.com/if/aroundtheworld"},
+    {"star":1, "title":"Underblood", "desc":"Mouse controlling blood flow.", "link":"https://www.newgrounds.com/portal/view/790698"},
+    {"star":0, "title":"News System", "desc":"Mass shooting poops.", "link":"https://www.newgrounds.com/portal/view/781776"},
+    {"star":0, "title":"January One", "desc":"Lost and found story.", "link":"https://www.newgrounds.com/portal/view/779484"},
+    {"star":0, "title":"Password System", "desc":"Pretending social media access.", "link":"https://www.newgrounds.com/portal/view/778623"},
+    {"star":0, "title":"AZ101ESB", "desc":"101 keys to press.", "link":"https://www.newgrounds.com/portal/view/766732"},
+    {"star":0, "title":"Frog Typing Tongue", "desc":"Frog's tongue made of texts.", "link":"https://www.newgrounds.com/portal/view/760187"},
+    {"star":1, "title":"Prism Light", "desc":"Mouse maze with a light.", "link":"https://www.newgrounds.com/portal/view/756495"},
+    {"star":1, "title":"Coordinator", "desc":"Pointing destinations. No bumps.", "link":"https://www.newgrounds.com/portal/view/753993"}
+    ]
+    };
+    
