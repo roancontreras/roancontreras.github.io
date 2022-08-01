f84def78fd2d3d1a6a6357f9736b5c1dc82ed6dd
Roan Contreras <roan.contreras@hotmail.com>
2022-08-01 14:42:15 +0800

    Optimize header and button UX

--- js/global.js
+++ js/global.js
@@ -1,3 +1,8 @@
 "use strict";
+var docTitle = [];
+docTitle = document.title.split('|');
+
+//header text + (select if home buttons or current page + back)
+document.getElementsByTagName("header")[0].innerHTML =  '<h1 style="margin-bottom: 0; padding-bottom: 0;">Roan Contreras</h1>\n<p style="text-align: center;">' + (docTitle.length == 1 ? '<a href="games.html">Games</a> / <a href="experiments.html">Experiments</a>  / roan.contreras [@] hotmail.com' : docTitle[1].trim() + " / " + '<a href="index.html">Home</a>') + "</p>";
 
 document.getElementsByTagName("footer")[0].innerHTML = '<p style="text-align: center;"><span id="privacyfooter"><a href="privacy.html">Privacy Policy</a> /</span> <a href="https://twitter.com/ToadieTechnika" target="_blank">Twitter</a></p>';
