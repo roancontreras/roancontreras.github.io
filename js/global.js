842dbce163925c53769bc8eebbecb64b41962831
Roan Contreras <roan.contreras@hotmail.com>
2022-08-01 10:29:45 +0800

    Organize js files and optimization

---
+++ js/global.js
@@ -0,0 +1,3 @@
+"use strict";
+
+document.getElementsByTagName("footer")[0].innerHTML = '<p style="text-align: center;"><span id="privacyfooter"><a href="privacy.html">Privacy Policy</a> /</span> <a href="https://twitter.com/ToadieTechnika" target="_blank">Twitter</a></p>';
