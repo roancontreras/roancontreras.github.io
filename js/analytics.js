e5fe9e8a563b31a2e244025ed3a6b822776530c9
Roan Contreras <toadfrogs@hotmail.com>
2023-09-08 14:43:16 +0800

    Optimize analytics, centered website and modified texts

---
+++ js/analytics.js
@@ -0,0 +1,10 @@
+"use strict";
+
+(function()
+{
+    document.getElementsByTagName("html")[0].innerHTML += "<script async src=\"https://www.googletagmanager.com/gtag/js?id=G-FZPF5LDCB2\"></script>";
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-FZPF5LDCB2');
+})();
